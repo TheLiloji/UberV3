617e23e8efae305ff9c6bf394857b14a46f11902
Zaccharie Bernard <zaccharieber@gmail.com>
2025-02-11 17:34:58 +0100

    Modification du CSS

---
+++ app/constants/Colors.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  background: '#f1f1f1',
+  categoryBackground: '#ffe4b9',
+  restaurantBackground: '#ffffff',
+  text: '#2d2f2f',
+  textGray: '#9b9c9c',
+  peach: '#ffe4b9',
+  orange: '#f6b44c',
+  tint: '#f6b44c',
+  icon: '#2d2f2f',
+  tabIconDefault: '#9b9c9c',
+  tabIconSelected: '#f6b44c',
+} as const;
